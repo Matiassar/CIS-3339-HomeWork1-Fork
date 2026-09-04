74a5014af05b74cf74bb52882f77f39adf86b24c
Matiassar <msanch74@cougarnet.uh.edu>
2026-09-04 15:18:42 -0500

    wired vues into router

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import CoursesView from '../views/CoursesView.vue'
+import EnrollmentsView from '../views/EnrollmentsView.vue'
+import StudentsView from '../views/StudentsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/courses',
+      name: 'Courses',
+      component: CoursesView
+    },
+    {
+      path: '/enrollments',
+      name: 'Enrollments',
+      component: EnrollmentsView
+    },
+    {
+      path: '/students',
+      name: 'Students',
+      component: StudentsView
+    }
+  ]
 })
 
 export default router
---
+++ frontend-vue/src/views/EnrollmentsView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Enrollments</div>
+</template>
