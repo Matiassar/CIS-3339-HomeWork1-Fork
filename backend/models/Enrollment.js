5f43268c46134e1224c7ae18814e66650fdfbb72
Matiassar <msanch74@cougarnet.uh.edu>
2026-09-04 17:21:24 -0500

    renamed old frontend file and added enrollment model

---
+++ backend/models/Enrollment.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const enrollmentSchema = new mongoose.Schema({
+    studentId: { type: String, required: true },
+    courseId: { type: String, required: true }
+});
+
+enrollmentSchema.index({ studentId: 1, courseId: 1 }, { unique: true });
+
+module.exports = mongoose.model('Enrollment', enrollmentSchema);
